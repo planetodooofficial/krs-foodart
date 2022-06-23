889dd942061c1e5a07bee817ff85034830b335f5
sunayana-kolekar <sunaina@planet-odoo.com>
2022-06-23 14:39:26 +0530

    invisible barcode field

--- custom/models/product_inherit.py
+++ custom/models/product_inherit.py
@@ -3,10 +3,10 @@ from odoo import api, fields, models, _
 class ProductInherit(models.Model):
     _inherit = 'product.template'
 
-    no_of_unit_in_carton=fields.Char('Number of units in a carton')
-    no_of_cartons_per_layer=fields.Char('Number of cartons per layer')
-    no_of_layers_full_pallet=fields.Char('Number of layers full pallet')
-    net_weight=fields.Char('Net weight')
-    gross_weight=fields.Char('Gross weight')
-    ean13=fields.Char('Single unit barcode ')
-    ean14=fields.Char('Carton barcode')
+    no_of_unit_in_carton=fields.Char('Number Of Units In a Carton')
+    no_of_cartons_per_layer=fields.Char('Number Of Cartons Per Layer')
+    no_of_layers_full_pallet=fields.Char('Number Of Layers Full Pallet')
+    net_weight=fields.Char('Net Weight')
+    gross_weight=fields.Char('Gross Weight')
+    ean13=fields.Char('Single Unit Barcode/ EAN13')
+    ean14=fields.Char('Carton Barcode/ EAN14')
